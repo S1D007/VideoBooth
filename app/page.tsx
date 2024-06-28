4a6831a3076e0546a95789d395a91d7006d7c422
Siddhant <s1d.official011001011@gmail.com>
2024-06-28 14:56:20 +0530

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,112 +1,143 @@
+"use client";
+import React, { useState } from "react";
 import Image from "next/image";
+import ReactPlayer from "react-player";
 
-export default function Home() {
+export default function Page() {
+  const [videoUrl, setVideoUrl] = useState<string | null>(null);
+  const [videoFile, setVideoFile] = useState<File | null>(null);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [qr, setQR] = useState<string | null>(null);
+  const handleFileChange = (event: any) => {
+    const file = event.target.files[0];
+    setVideoFile(file);
+    if (file && file.type.startsWith("video/")) {
+      const videoBlob = URL.createObjectURL(file);
+      setVideoUrl(videoBlob);
+    } else {
+      alert("Please select a valid video file.");
+    }
+  };
+
+  const uplaodFile = async (file: File | null) => {
+    if (!file) {
+      alert("Please select a valid video file.");
+      return;
+    }
+    const formData = new FormData();
+    formData.append("image", file);
+    setLoading(true);
+    try {
+      const response = await fetch(
+        "https://api.gokapturehub.com/upload/single",
+        {
+          method: "POST",
+          body: formData,
+        }
+      );
+      const data = await response.json();
+      const currentHostname = `${window.location.protocol}//${window.location.hostname}`;
+      const QRString = `${currentHostname}/download/${encodeURIComponent(
+        data.data
+      )}`;
+      alert("QR Code Generated. Click OK to download the QR Code.");
+      console.log(QRString);
+      setQR(
+        `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${QRString}`
+      );
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+    <main className="flex flex-col items-center justify-center h-screen space-y-4">
+      <div className="w-full max-w-md">
+        {!qr ? (
+          <>
+            <h1 className="text-gray-900 font-semibold text-2xl text-center">
+              Upload Video to Generate{" "}
+              <span className="font-extrabold text-3xl text-gray-500">QR</span>{" "}
+              Code
+            </h1>
+            <>
+              {videoUrl ? (
+                <>
+                  <div className="relative w-full h-96">
+                    <ReactPlayer
+                      url={videoUrl}
+                      controls
+                      width="100%"
+                      height="100%"
+                      className="absolute top-0 left-0"
+                    />
+                  </div>
+                  <button
+                    onClick={() => uplaodFile(videoFile)}
+                    disabled={loading}
+                    className="absolute bottom-4 right-4 bg-gray-900 text-white font-semibold px-4 py-2 rounded-lg"
+                  >
+                    {loading ? "Generating QR... Please Wait" : "Generate QR"}
+                  </button>
+                </>
+              ) : (
+                <label
+                  htmlFor="uploadFile1"
+                  className="bg-white text-gray-500 font-semibold text-base rounded-lg w-full h-52 flex flex-col items-center justify-center cursor-pointer border-2 border-gray-300 border-dashed mt-4"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="w-11 mb-2 fill-gray-500"
+                    viewBox="0 0 32 32"
+                  >
+                    <path
+                      d="M23.75 11.044a7.99 7.99 0 0 0-15.5-.009A8 8 0 0 0 9 27h3a1 1 0 0 0 0-2H9a6 6 0 0 1-.035-12 1.038 1.038 0 0 0 1.1-.854 5.991 5.991 0 0 1 11.862 0A1.08 1.08 0 0 0 23 13a6 6 0 0 1 0 12h-3a1 1 0 0 0 0 2h3a8 8 0 0 0 .75-15.956z"
+                      data-original="#000000"
+                    />
+                    <path
+                      d="M20.293 19.707a1 1 0 0 0 1.414-1.414l-5-5a1 1 0 0 0-1.414 0l-5 5a1 1 0 0 0 1.414 1.414L15 16.414V29a1 1 0 0 0 2 0V16.414z"
+                      data-original="#000000"
+                    />
+                  </svg>
+                  Upload video file
+                  <input
+                    type="file"
+                    id="uploadFile1"
+                    className="hidden"
+                    accept="video/*"
+                    onChange={handleFileChange}
+                  />
+                  <p className="text-xs font-medium text-gray-400 mt-2">
+                    MP4, AVI, MKV are allowed.
+                  </p>
+                </label>
+              )}
+            </>
+          </>
+        ) : (
+          <>
+            <h1 className="text-gray-900 font-semibold text-2xl text-center mb-4">
+              Upload Video{" "}
+              <span className="font-extrabold text-3xl text-gray-500">QR</span>{" "}
+              Code Generated
+            </h1>
+            <div className="w-full flex flex-col items-center justify-center space-y-2">
+              <img src={qr} className="w-52 h-52" />
+              <button
+                onClick={() => {
+                  setQR(null);
+                  setVideoUrl(null);
+                  setVideoFile(null);
+                }}
+                className="absolute bottom-4 right-4 bg-gray-900 text-white font-semibold px-4 py-2 rounded-lg"
+              >
+                Regenerate QR
+              </button>
+            </div>
+          </>
+        )}
       </div>
     </main>
   );
